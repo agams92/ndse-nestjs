47923e51854fc8168e6e6ec7af78f9d053464615
Lev <l.belonovskiy@onpoint.ru>
2021-05-12 19:51:54 +0300

    добавил exception

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './exceptions';
 import { WrapperInterceptor } from './interceptors';
 
 const port = process.env.PORT
@@ -7,6 +8,7 @@ const port = process.env.PORT
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new WrapperInterceptor());
+  app.useGlobalFilters(new HttpExceptionFilter())
   await app.listen(port);
   console.log(`App is ready on port:${port}`)
 }
